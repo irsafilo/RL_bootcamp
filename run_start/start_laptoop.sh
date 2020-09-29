eba407b933913f69d76512c205994899bd51dd84
irsafilo <irsafilo@gmail.com>
2020-09-29 23:19:28 +0300

    unix code

--- run_start/start_laptoop.sh
+++ run_start/start_laptoop.sh
@@ -4,7 +4,7 @@ libsdl2-image-dev libsdl2-ttf-dev libsdl2-gfx-dev libboost-all-dev \
 libdirectfb-dev libst-dev mesa-utils xvfb x11vnc libsdl-sge-dev python3-pip
 git clone https://github.com/google-research/football.git
 cd football
-sudo apt-get -y install python3-venv
+sudo apt-get install python3-venv
 python3 -m venv football-env
 source football-env/bin/activate
 pip3 install scikit-build
